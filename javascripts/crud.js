f38c955f42d9695c70e90db9bc6d18f5aa6cb8fd
Sanyyouisf <sanymamdoh@yahoo.com>
2017-05-01 18:42:35 -0500

    delete function

--- javascripts/crud.js
+++ javascripts/crud.js
@@ -1,16 +1,24 @@
 //augmenter
 var FbApi = ((oldCrap) => {
-	//
+	//this function convert the object to an array and loop through it and push the object to the array
 	oldCrap.getTodos = () =>{
 		let items =[];
 		return new Promise ((resolve,reject) => {
 			$.ajax ("./database/seed.json")
 			.done((data)=>{
+				//response here is object  of items
 				let response = data.items;
+				console.log("response",response);
+				//Object.keys convert the response object to an array 
+				//forEach will loop through this aray item0,item1,item2
 				Object.keys(response).forEach((key) =>{
 					console.log("key",key);
 					response[key].id = key;
+					//adding new item to the array that have (item.id=0 for example)
+					//and push it to the array
 					items.push(response[key]);
+					console.log("response[key]",response[key]);
+					console.log("items",items);
 				});
 				FbApi.setTodos(items);
 				resolve();
@@ -25,13 +33,16 @@ var FbApi = ((oldCrap) => {
 
 	oldCrap.addTodo = (newTodo) => {
 		return new Promise ((resolve,reject) => {
+			//newTodo.id =item +the number of items in the todo array (for example item3).
 			newTodo.id = `item${FbApi.todoGetter().length}`;
 			console.log("newTodo",newTodo);
+			//push the new item (with the id:item3 for example)to the todo array. 
 			FbApi.setSingleTodo(newTodo);
 			resolve();
 		});
 	};
 
+	//check for the item is completed or not 
 	oldCrap.checker = (id) => {
 		return new Promise ((resolve,reject) => {
 			FbApi.setChecker(id);
@@ -39,6 +50,13 @@ var FbApi = ((oldCrap) => {
 		});
 	};
 
+	//delete the item from the array
+	oldCrap.deleteTodo = (id) => {
+		return new Promise ((resolve,reject) => {
+			FbApi.duhlete(id);
+			resolve();
+		});
+	};
 
 
 	return oldCrap;
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function(){
 	
+	//when clicking on new it hide the list-container and show new-container  
 	$("#new-item").click(() => {
 		$(".list-container").addClass("hide");
 		$(".new-container").removeClass("hide");
 
 	});
 
+	//when clicking on new it hide the new-container and show list-container  
 	$("#list-items").click(() => {
 		$(".new-container").addClass("hide");
 		$(".list-container").removeClass("hide");
@@ -22,7 +24,9 @@ $(document).ready(function(){
 	});
 
 	//add todo
+	//when writing in the text box of the new page
 	$("#add-todo-button").click(() => {
+		//add new object that is not completed (isCompleted:false) and will add to to do list .
 		let newTodo = {
 			isCompleted:false,
 			task:$("#add-todo-text").val()
@@ -41,6 +45,14 @@ $(document).ready(function(){
 	});
 
 	// delete todo
+	$(".main-container").on("click",'.delete',(event)=> {
+		FbApi.deleteTodo(event.target.id).then(()=>{
+			FbApi.writeDom();
+			countTask();
+		}).catch((error)=>{
+			console.log(" error in deleteTodo",error);
+		});
+	});
 	
 
 
@@ -49,8 +61,10 @@ $(document).ready(function(){
 	//complete todo 
 	$(".main-container").on("click", 'input[type="checkbox"]',(event)=>{
 		console.log("id",event.target.id);
+			//convert the item that you click on to be completed .
 		FbApi.checker(event.target.id).then(()=>{
 			FbApi.writeDom();
+			//update the counter
 			countTask();
 		}).catch((error)=>{
 			console.log("checker error",error);
@@ -60,7 +74,9 @@ $(document).ready(function(){
 
 
 	let countTask = () => {
+		//number of uncompleted tasks(in todo list).
 		let remainingTasks = $("#incompleted-tasks li").length;
+		//every 300 sec will fadein with the new number of uncompleted.
 		$("#counter").hide().fadeIn(300).html(remainingTasks);
 	};
 
